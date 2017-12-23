ea7d4edd998bc7f03022a259935c1c84ce855fe4
alexander <Aleksandr.Pimenov@lanit-tercom.com>
2017-12-23 19:35:45 +0300

    Fixing misprint

--- bCNC.py
+++ bCNC.py
@@ -2445,10 +2445,10 @@ class Application(Toplevel,Sender):
                         if data[1] == 22:
                             self.stopRun();
                             
-                        if data[i] == 10:
+                        if data[1] == 10:
                             print "Step should change to 0.01"
                             self.control.setStep(0.01);
-                        if data[i] == 11:
+                        if data[1] == 11:
                             print "Step should change to 0.1"
                             self.control.setStep(0.1);
                             
